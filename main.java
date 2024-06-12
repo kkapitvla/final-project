0c70b12000f3678c270a5c1824cff9315700aa2e
kkapitvla <161339763+kkapitvla@users.noreply.github.com>
2024-06-12 17:25:18 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    double wzrost = 170;
+    double waga = 80;
+    double BMI = (waga / ((wzrost / 100) * (wzrost / 100)));
+System.out.println("Twój wskaźnik BMI to:" + BMI);
+  }}
